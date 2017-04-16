592064ee65108a0d24df48d77fdd70b9948e4efd
ErikJanssen61 <erik.janssen61@gmail.com>
2017-04-16 12:10:11 +0200

    Number 4 - Move whole package - 2 neww classes added

---
+++ AllMyCode/src/nextGenPackage/EchoLine.java
@@ -0,0 +1,16 @@
+package nextGenPackage;
+import java.util.Scanner;
+
+/*
+05-01
+*/
+
+class EchoLine {
+
+    public static void main(String args[]) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println(myScanner.nextLine());
+        myScanner.close();
+    }
+}
